da93395369743ffcc6291adb56d7f499807ac830
ivmak <ivan.r.makarov@yandex.ru>
2025-12-30 15:25:28 +0300

    FIX -- Main activity 2

--- app/src/main/java/otus/homework/coroutines/MainActivity.kt
+++ app/src/main/java/otus/homework/coroutines/MainActivity.kt
@@ -14,7 +14,6 @@ class MainActivity : AppCompatActivity() {
 
     private val viewModel by viewModels<CatsViewModel>()
 
-    private lateinit var button: Button
     private lateinit var imgView: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
