f0320e7e20306f6112486898e5109cf30c6ebb93
ivmak <ivan.r.makarov@yandex.ru>
2025-12-30 17:22:42 +0300

    FIX -- Main Activity/ Remove unused

--- app/src/main/java/otus/homework/coroutines/MainActivity.kt
+++ app/src/main/java/otus/homework/coroutines/MainActivity.kt
@@ -23,8 +23,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         button = findViewById(R.id.button)
-        textView = findViewById(R.id.fact_textView)
-        imgView = findViewById(R.id.random_imgView)
 
         viewModel.onInitComplete()
 
